69f2ce98e9cd3ed3e65b64b6977d846b2790fe01
jamiul <jamiulewu24@gmail.com>
2021-02-25 13:19:49 +0600

    The Reply Form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 col-md-offset-2">
             <div class="card">
                 <div class="card-header">
                     <a href="#">{{ $thread->creator->name }}</a>
@@ -17,11 +17,29 @@
         </div>
     </div>
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 col-md-offset-2">
             @foreach($thread->replies as $reply)
                 @include('threads.reply')
             @endforeach
         </div>
     </div>
+
+    @if(auth()->check())
+    <div class="row justify-content-center">
+        <div class="col-md-8 col-md-offset-2">
+            <form action="{{$thread->path().'/replies'}}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="name" class="col-form-label">{{ __('Reply') }}</label>
+                    <textarea name="body" class="form-control" placeholder="Have Something to Say" rows="5"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-primary">Post</button>
+            </form>
+        </div>
+    </div>
+    @else
+    <p class="text-center">Please  <a href="{{route('login')}}">sign in</a> to participate in this discussion</p>
+    @endif
 </div>
 @endsection
