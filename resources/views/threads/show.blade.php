b837f5488b1c60342a9205a36349793ec83facd8
jamiul <jamiulewu24@gmail.com>
2022-07-18 14:07:33 +0600

    30. A User's Activity Feed Should Include Favorited

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,7 +5,7 @@
     {{-- @dd('thread.show'); --}}
     <div class="row justify-content-center">
         <div class="col-md-8 col-md-offset-2">
-            <div class="card">
+            <div class="card mt-4">
                 <div class="card-header">
 
                     <div class="level">
