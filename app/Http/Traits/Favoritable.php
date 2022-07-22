9ab15f425d82ca6fb7f63eee22d1c37dba1ca321
jamiul <jamiulewu24@gmail.com>
2022-07-22 15:38:18 +0600

    35. Squashing Bugs

---
+++ app/Http/Traits/Favoritable.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Traits;
+
+use App\Models\Favorite;
+
+trait Favoritable
+{
+    /**
+     * Boot the trait.
+     */
+    protected static function bootFavoritable()
+    {
+        static::deleting(function ($model) {
+            $model->favorites->each->delete();
+        });
+    }
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    public function favorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+        if (!$this->favorites()->where($attributes)->exists()) {
+            return $this->favorites()->create($attributes);
+        }
+    }
+
+    /**
+     * Unfavorite the current reply.
+     */
+    public function unfavorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+
+        $this->favorites()->where($attributes)->get()->each->delete();
+    }
+
+    public function isFavorited()
+    {
+        return !!$this->favorites()->where('user_id', auth()->id())->count();
+    }
+
+    /**
+     * Fetch the favorited status as a property.
+     *
+     * @return bool
+     */
+    public function getIsFavoritedAttribute()
+    {
+        return $this->isFavorited();
+    }
+
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites->count();
+    }
+}
--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Http\Traits\Favoritable;
 use App\Http\Traits\RecordsActivity;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Reply extends Model
 {
-    use HasFactory, RecordsActivity;
+    use HasFactory, Favoritable, RecordsActivity;
 
     protected $guarded = [];
 
@@ -23,49 +24,6 @@ class Reply extends Model
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
-    }
-
-    public function favorites()
-    {
-        return $this->morphMany(Favorite::class, 'favorited');
-    }
-
-    public function favorite()
-    {
-        $attributes = ['user_id' => auth()->id()];
-        if (!$this->favorites()->where($attributes)->exists()) {
-            return $this->favorites()->create($attributes);
-        }
-    }
-
-    /**
-     * Unfavorite the current reply.
-     */
-    public function unfavorite()
-    {
-        $attributes = ['user_id' => auth()->id()];
-
-        $this->favorites()->where($attributes)->delete();
-    }
-
-    public function isFavorited()
-    {
-        return !!$this->favorites()->where('user_id', auth()->id())->count();
-    }
-
-    /**
-     * Fetch the favorited status as a property.
-     *
-     * @return bool
-     */
-    public function getIsFavoritedAttribute()
-    {
-        return $this->isFavorited();
-    }
-
-    public function getFavoritesCountAttribute()
-    {
-        return $this->favorites->count();
     }
 
     public function thread()
