ba5239eeb589b0cbcf377c52dca6ab7a66709193
jamiul <jamiulewu24@gmail.com>
2022-08-02 15:44:05 +0600

    37. A New Reply Component

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -25,10 +25,14 @@ class ReplyController extends Controller
         ]);
 
 
-        $thread->addReply([
+        $reply = $thread->addReply([
             'body' => $request->body,
             'user_id' => auth()->id()
         ]);
+
+        if (request()->expectsJson()) {
+            return $reply->load('owner');
+        }
 
         return back()->with('flash', 'Your reply has been left.');
     }
